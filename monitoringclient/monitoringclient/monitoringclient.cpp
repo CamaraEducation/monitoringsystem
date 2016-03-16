227fae54b88fccf4d8a84b3715346f6a890c5583
sduignan <sduignan@tcd.ie>
2016-03-16 17:00:59 +0000

    Writes active window title to console once every 10 seconds, 10 times

--- monitoringclient/monitoringclient/monitoringclient.cpp
+++ monitoringclient/monitoringclient/monitoringclient.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 
 int main()
 {
+	for (int i = 0; i < 10; i++) {
+		Sleep(10000);
+
+		HWND handle = GetForegroundWindow();
+		int bufsize = GetWindowTextLength(handle) + 1;
+		LPWSTR title = new WCHAR[bufsize];
+		GetWindowText(handle, title, bufsize);
+		
+		wcout << title << endl;
+
+		delete title;
+	}
+
+	char waitchar;
+
+	cin >> waitchar;
+
     return 0;
 }
 
