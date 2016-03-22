8c87a5ee54a28ed0f0b12b0bc78cd3d24c8cd49f
sduignan <sduignan@tcd.ie>
2016-03-22 13:09:24 +0000

    Small fix, no longer breaks on em dash

--- monitoringclient/monitoringclient/monitoringclient.cpp
+++ monitoringclient/monitoringclient/monitoringclient.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fcntl.h>
+#include <io.h>
 #include <string>
 
 using namespace std;
@@ -10,17 +12,21 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i < 10; i++) {
-		Sleep(10000);
+	_setmode(_fileno(stdout), _O_U16TEXT);
+
+	for (int i = 0; i < 50; i++) {
+		Sleep(4000);
 
 		HWND handle = GetForegroundWindow();
-		int bufsize = GetWindowTextLength(handle) + 1;
-		LPWSTR title = new WCHAR[bufsize];
-		GetWindowText(handle, title, bufsize);
-		
-		wcout << title << endl;
 
-		delete title;
+		if (handle){
+			int bufsize = GetWindowTextLength(handle) + 1;
+			LPWSTR title = new WCHAR[bufsize];
+			GetWindowText(handle, title, bufsize);
+			wprintf(L"%s\n", title);
+
+			delete [] title;
+		}
 	}
 
 	char waitchar;
